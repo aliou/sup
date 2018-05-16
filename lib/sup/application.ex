ca654d894876ba3318c10fc43410b2d1f5e2e586
Aliou Diallo <code@aliou.me>
2018-05-16 11:06:18 +0200

    Start the scheduler with the application.

--- lib/sup/application.ex
+++ lib/sup/application.ex
@@ -4,7 +4,9 @@ defmodule Sup.Application do
   use Application
 
   def start(_type, _args) do
-    children = []
+    children = [
+      Sup.Scheduler
+    ]
 
     opts = [strategy: :one_for_one, name: Sup.Supervisor]
     Supervisor.start_link(children, opts)
